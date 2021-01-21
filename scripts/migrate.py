2c95905c5099fd568d4d4775b431e970353e04ee
Raymond Thurman <raymondthurman5@gmail.com>
2021-01-20 20:13:54 -0500

    minor alteration to letter

--- scripts/migrate.py
+++ scripts/migrate.py
@@ -102,7 +102,7 @@ with open(masterFile) as master, open(outputFileName, 'w') as outfile:
         # This will be a better way in the future to do all of this
         # Right now this just overwrites race to the proper item
         if newRow[9] == 'c. Chose not to respond':
-            newRow[9] = 'd. Chose not to respond'
+            newRow[9] = 'k. Chose not to respond'
 
         w.writerow(newRow)
 
