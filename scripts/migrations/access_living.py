5bbd3e67a0898f41576a6ca722c5ad889bcb2fab
Raymond Thurman <raymondthurman5@gmail.com>
2021-04-09 10:20:38 -0400

    Starting script for access living migration

---
+++ scripts/migrations/access_living.py
@@ -0,0 +1,9 @@
+# Specific file where all of Access Livings data will be manipulated.
+# Open all needed files
+# Grab correct columns from every file to fill our template
+# Create a hash table of clients using ClientID as the Hash table Key
+# (Columns with options will be corrected after populating our template)
+# Create a hash table off of the dropdown options file using the option for the Hash table Key
+# Correct columns that are using options by utilizing the created hash table
+# Once all columns are populated then the regular process of updating the data to match our points starts.
+
